5073ed854f79570fac462a840700b0b21d1ef027
RamathabatheN <mashoto23@gmail.com>
2023-09-25 08:34:49 -0700

    updates

--- main.h
+++ main.h
@@ -1,32 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define BUFF_SIZE (1024)
 
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
 #include <limits.h>
-#include <stddef.h>
 #include <stdint.h>
 
-/**
- * struct sign - Struct sign
- *
- * @sign: The operator
- * @f: The function associated
- */
-/*typedef struct sign
-{
-char *signed;
-int (*f)(int a);
-} sig_n;*/
-
-void prints(const char *format, va_list more);
+int prints(const char *format, va_list more);
 int _putchar(char c);
 int _abs(int n);
 void _swap(char *x, char *y);
 char *_reverse(char *buf, int a, int b);
 void rev_string(char *str);
 size_t _strlen(const char *st);
+int _strlens(const char *st);
 char *_itoa(int given, char *buf, int base);
 int sign_decimal(int n);
 int num_binary(unsigned int n);
@@ -36,8 +25,12 @@ int sign_hexa(unsigned int n);
 int sign_HEXA(unsigned int n);
 char sign_per(void);
 int sign_char(char s);
-char *sign_string(char *s);
+ssize_t sign_string(char *s);
 int _printf(const char *format, ...);
 void print_buf(char buffer[], int *buff_ind);
+int print_rot13string(char *str);
+void addresses(void *i);
+int non_printable(char *buffer);
+int sign_HEXAs(unsigned int n);
 
 #endif
